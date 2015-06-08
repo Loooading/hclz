39a4f14dc558b866b34a9f594d6de860db2cb87f
Loooading <lhj115709@163.com>
2015-06-08 14:21:16 +0800

    Initial Commit

---
+++ HaoCLZ/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HaoCLZ
+//
+//  Created by Loading on 15/6/8.
+//  Copyright (c) 2015年 liuhj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
